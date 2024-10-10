c3361a0d1746d7c5b70dde51125120e2d255b7eb
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-10-10 15:12:01 -0300

    Finalizando Módulo Estrutura de Repeticao com Exemplo de Do While

---
+++ Java-Completo-Programacao-Orientada-a-Objetos/src/EstruturaDeRepeticao/DoWhile.java
@@ -0,0 +1,20 @@
+package EstruturaDeRepeticao;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        char resp = 's';
+
+        do {
+            System.out.println("Escreva a temperatura em Celsius");
+            double C = s.nextDouble();
+            double F = (9.0 * C) / (5.0 + 32.0);
+            System.out.printf("Equivalente a Fahrenheit: %.1f%n", F);
+            System.out.println("Deseja Repetir o Programa?");
+            resp = s.next().charAt(0);
+        } while(resp != 'n');
+    }
+}
