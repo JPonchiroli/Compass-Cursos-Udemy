813339a38e1785a5aa4f00a0bc08514f2d57f208
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-10-17 16:51:49 -0300

    Primeira classe Projeto Xadrez - Posicao

---
+++ Java-Completo-Programacao-Orientada-a-Objetos/src/ProjetoXadrez/Aplicacao/Main.java
@@ -0,0 +1,11 @@
+package ProjetoXadrez.Aplicacao;
+
+import ProjetoXadrez.Tabuleiro.Posicao;
+
+public class Main {
+    public static void main(String[] args) {
+        Posicao posicao = new Posicao(5, 6);
+
+        System.out.println(posicao);
+    }
+}
