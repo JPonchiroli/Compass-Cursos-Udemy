7788d4cca82195c40689fe5129d6b0206596a8ff
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-10-24 15:57:29 -0300

    Adicionando Select e Insert

---
+++ Java-Completo-Programacao-Orientada-a-Objetos/src/Bonus/AcessoBancoDeDadosJDBC/Aplicacao/Insert.java
@@ -0,0 +1,53 @@
+package Bonus.AcessoBancoDeDadosJDBC.Aplicacao;
+
+import Bonus.AcessoBancoDeDadosJDBC.db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Insert {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "INSERT INTO seller"
+                      + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                      + "VALUES "
+                      + "(?, ?, ?, ?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id: " + id + " added");
+                }
+            } else {
+                System.out.println("No rows Affected");
+            }
+
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace();
+        } finally {
+            DB.fecharStatement(st);
+            DB.fecharConexao();
+        }
+    }
+}
--- Java-Completo-Programacao-Orientada-a-Objetos/src/Bonus/AcessoBancoDeDadosJDBC/db/DB.java
+++ Java-Completo-Programacao-Orientada-a-Objetos/src/Bonus/AcessoBancoDeDadosJDBC/db/DB.java
@@ -2,9 +2,7 @@ package Bonus.AcessoBancoDeDadosJDBC.db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -45,4 +43,25 @@ public class DB {
             throw new DbException(e.getMessage());
         }
     }
+
+    public static void fecharStatement(Statement st){
+        if (st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void fecharResultSet(ResultSet rs){
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
 }
