e8474cf3c02c2797a771b271934e22f33c7e8a1d
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-10-24 16:41:43 -0300

    Criando classe Seller em DemoDAO

--- Java-Completo-Programacao-Orientada-a-Objetos/src/Bonus/AcessoBancoDeDadosJDBC/DemoDAO/application/Main.java
+++ Java-Completo-Programacao-Orientada-a-Objetos/src/Bonus/AcessoBancoDeDadosJDBC/DemoDAO/application/Main.java
@@ -1,11 +1,16 @@
 package Bonus.AcessoBancoDeDadosJDBC.DemoDAO.application;
 
 import Bonus.AcessoBancoDeDadosJDBC.DemoDAO.model.entities.Department;
+import Bonus.AcessoBancoDeDadosJDBC.DemoDAO.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
+        Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dp);
 
         System.out.println(dp);
+        System.out.println(sl);
     }
 }
