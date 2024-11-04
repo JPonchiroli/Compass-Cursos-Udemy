d0157150e88d334f9b2fbb6f756f513ec4b441e2
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-04 14:06:48 -0300

    Adicinando base do projeto webservice

---
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/ProjetoWebServicesComSpringBootEJpaApplication.java
@@ -0,0 +1,13 @@
+package com.pbcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesComSpringBootEJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesComSpringBootEJpaApplication.class, args);
+	}
+
+}
---
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/test/java/com/pbcompass/ProjetoWebServicesComSpringBootEJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbcompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComSpringBootEJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
