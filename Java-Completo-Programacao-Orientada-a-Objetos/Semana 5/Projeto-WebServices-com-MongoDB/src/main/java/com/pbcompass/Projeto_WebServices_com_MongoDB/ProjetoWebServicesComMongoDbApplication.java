491291700c630c9acafb7ac1783d7628262e837f
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-05 17:28:46 -0300

    Adicionando base do projeto WebService com MongoDB

---
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-MongoDB/src/main/java/com/pbcompass/Projeto_WebServices_com_MongoDB/ProjetoWebServicesComMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.pbcompass.Projeto_WebServices_com_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesComMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesComMongoDbApplication.class, args);
+	}
+
+}
---
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-MongoDB/src/test/java/com/pbcompass/Projeto_WebServices_com_MongoDB/ProjetoWebServicesComMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbcompass.Projeto_WebServices_com_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
