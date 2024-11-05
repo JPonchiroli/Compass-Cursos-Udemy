638c78aaac7522b22310ed244fdf3758198dc306
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-05 16:28:24 -0300

    Implementando CRUD Category

--- Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/services/CategoryService.java
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/services/CategoryService.java
@@ -1,9 +1,15 @@
 package com.pbcompass.services;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
 import com.pbcompass.entities.Category;
 import com.pbcompass.repositories.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.pbcompass.services.exceptions.DatabaseException;
+import com.pbcompass.services.exceptions.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +26,35 @@ public class CategoryService {
 
     public Category findById(Long id){
         Optional<Category> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Category insert(Category obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Category update(Long id, Category obj){
+        try {
+            Category entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Category entity, Category obj) {
+        entity.setName(obj.getName());
     }
 
 }
