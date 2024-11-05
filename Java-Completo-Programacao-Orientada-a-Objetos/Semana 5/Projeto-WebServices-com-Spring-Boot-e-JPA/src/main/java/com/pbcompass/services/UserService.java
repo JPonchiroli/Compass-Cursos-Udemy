61c474f64d2fbd62b7ae69c40c20f126c8010ad2
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-05 15:55:29 -0300

    Implementando Lindando com excecao findById()

--- Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/services/UserService.java
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/services/UserService.java
@@ -1,5 +1,6 @@
 package com.pbcompass.services;
 
+import com.pbcompass.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
