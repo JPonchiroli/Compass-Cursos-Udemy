de03c87abe861c30a9acb2a222bb899b651f923a
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-05 16:03:18 -0300

    Implementando Lindando com excecao delete()

--- Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/services/UserService.java
+++ Java-Completo-Programacao-Orientada-a-Objetos/Semana 5/Projeto-WebServices-com-Spring-Boot-e-JPA/src/main/java/com/pbcompass/services/UserService.java
@@ -1,7 +1,10 @@
 package com.pbcompass.services;
 
+import com.pbcompass.services.exceptions.DatabaseException;
 import com.pbcompass.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.pbcompass.entities.User;
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
