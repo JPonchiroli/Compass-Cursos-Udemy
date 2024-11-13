18f542359e1accb8043095161343b06c99df9760
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-13 16:47:40 -0300

    Documentando o recurso para criar um novo usuário

--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/controller/UserController.java
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/controller/UserController.java
@@ -4,6 +4,12 @@ import com.pbcompass.park_api.web.dto.UserCreateDto;
 import com.pbcompass.park_api.web.dto.UserPasswordDto;
 import com.pbcompass.park_api.web.dto.UserResponseDto;
 import com.pbcompass.park_api.web.dto.mapper.UserMapper;
+import com.pbcompass.park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +20,9 @@ import org.springframework.web.bind.annotation.*;
 import com.pbcompass.park_api.entities.User;
 import com.pbcompass.park_api.services.UserService;
 
-import java.util.Arrays;
 import java.util.List;
 
+    @Tag(name = "Users", description = "It has all the operations related to the insertion, editing and display resources of a user")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
@@ -24,6 +30,15 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Insert a new User", description = "Resource to create a new user",
+        responses = {
+            @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "User e-mail already registered in the system",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "422", description = "Resource not processed buy wrong data entry",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+    })
     @PostMapping
     public ResponseEntity<UserResponseDto> insert(@Valid @RequestBody UserCreateDto createDto) {
         User newUser = userService.save(UserMapper.toUser(createDto));
