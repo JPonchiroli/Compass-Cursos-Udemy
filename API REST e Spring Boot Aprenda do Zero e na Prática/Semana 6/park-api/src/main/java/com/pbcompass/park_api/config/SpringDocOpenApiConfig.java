5669d90e2b6b31610438235a475c2efe0671654e
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-13 16:36:49 -0300

    Implementando base para documentacao com Swagger

---
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.pbcompass.park_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("REST API - Spring Park")
+                                .description("An API for managing a vehicle parking lot")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("""
+                                        https://www.apache.org/licenses/LICENSE-2.0"""))
+                                .contact(new Contact().name("João Ponchiroli").email("joaopedroponqueroli@gmail.com"))
+                );
+    }
+
+}
