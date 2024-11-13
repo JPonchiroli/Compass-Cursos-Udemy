ccc31eae834a9644a27e1f1ea1ffe3b0d47b2e80
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-13 16:17:01 -0300

    Implementando validacao password

--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/services/UserService.java
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.pbcompass.park_api.services;
 
 import com.pbcompass.park_api.exception.EntityNotFoundException;
+import com.pbcompass.park_api.exception.PasswordInvalidException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -36,12 +37,12 @@ public class UserService {
     @Transactional
     public User updatePassword(Long id, String currentPassword, String newPassword, String confirmPassword){
         if (!newPassword.equals(confirmPassword)){
-            throw new RuntimeException("The new password and confirmation field are not equals");
+            throw new PasswordInvalidException("The new password and confirmation field are not equals");
         }
 
         User user = findById(id);
         if (!user.getPassword().equals(currentPassword)){
-            throw new RuntimeException("Your password does not match");
+            throw new PasswordInvalidException("Your password does not match");
         }
         user.setPassword(newPassword);
         return user;
--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/exception/ApiExceptionHandler.java
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.pbcompass.park_api.web.exception;
 
 import com.pbcompass.park_api.exception.EntityNotFoundException;
+import com.pbcompass.park_api.exception.PasswordInvalidException;
 import com.pbcompass.park_api.exception.UsernameUniqueViolationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ValidationException;
@@ -44,5 +45,14 @@ public class ApiExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(PasswordInvalidException.class)
+    public ResponseEntity<ErrorMessage> passwordInvalidException(PasswordInvalidException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
     }
 }
