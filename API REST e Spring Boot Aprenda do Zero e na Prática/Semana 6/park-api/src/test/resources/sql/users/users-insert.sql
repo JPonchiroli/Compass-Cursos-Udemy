1dc26b3f2688efe1451a9ef1dd4fa7fa859c6f38
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-14 16:36:28 -0300

    Implementando base para testes da aplicacao

---
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,4 @@
+insert into tb_users (id, username, password, role)
+    values (100, "ana@gmail.com", "123456", "ROLE_ADMIN"),
+            (101, "bia@gmail.com", "123456", "ROLE_CLIENT"),
+            (102, "bob@gmail.com", "123456", "ROLE_CLIENT");
