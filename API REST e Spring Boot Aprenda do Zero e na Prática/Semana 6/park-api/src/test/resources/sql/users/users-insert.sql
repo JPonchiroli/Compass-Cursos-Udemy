0cb9ba3ba2df572ae807645f1bb406e548158f02
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-18 14:24:45 -0300

    Teste para criar usuario - Exception: CannotReadScriptException

--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/test/resources/sql/users/users-insert.sql
+++
@@ -1,4 +0,0 @@
-insert into tb_users (id, username, password, role)
-    values (100, "ana@gmail.com", "123456", "ROLE_ADMIN"),
-            (101, "bia@gmail.com", "123456", "ROLE_CLIENT"),
-            (102, "bob@gmail.com", "123456", "ROLE_CLIENT");
