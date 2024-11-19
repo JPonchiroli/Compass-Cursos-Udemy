4bff9f197d0e3d2f73ae569dee04eab13680835f
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-19 13:45:54 -0300

    Implementando controller para autenticacao

---
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/dto/UserLoginDto.java
@@ -0,0 +1,21 @@
+package com.pbcompass.park_api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserLoginDto {
+
+    @NotBlank
+    @Email(message = "Invalid email format", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
