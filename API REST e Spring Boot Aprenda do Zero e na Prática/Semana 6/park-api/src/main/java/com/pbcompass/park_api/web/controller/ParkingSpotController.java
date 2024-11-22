16ddb1760da094edbeab898a973a3da8867d6a95
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-22 15:55:30 -0300

    Implementando documentacao para ParkingSpot

--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/controller/ParkingSpotController.java
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/web/controller/ParkingSpotController.java
@@ -5,8 +5,16 @@ import com.pbcompass.park_api.services.ParkingSpotService;
 import com.pbcompass.park_api.web.dto.ParkingSpotCreateDto;
 import com.pbcompass.park_api.web.dto.ParkingSpotResponseDto;
 import com.pbcompass.park_api.web.dto.mapper.ParkingSpotMapper;
+import com.pbcompass.park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +29,37 @@ public class ParkingSpotController {
 
     private final ParkingSpotService parkingSpotService;
 
+
+    @Operation(
+            summary = "Create a new parking spot",
+            description = "Endpoint to create a new parking spot. Request requires a bearer token. Access restricted to Role='ADMIN'.",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Resource successfully created",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL of the created resource")
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Parking spot already registered",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Unprocessable entity due to missing or invalid data",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied for CLIENT role",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            }
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> insert(@RequestBody @Valid ParkingSpotCreateDto dto) {
@@ -33,6 +72,31 @@ public class ParkingSpotController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(
+            summary = "Find a parking spot",
+            description = "Endpoint to retrieve a parking spot by its code. Request requires a bearer token. Access restricted to Role='ADMIN'.",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Resource successfully found",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ParkingSpotResponseDto.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Parking spot not found",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied for CLIENT role",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            }
+    )
     @GetMapping("/{code}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ParkingSpotResponseDto> findByCode(@PathVariable String code) {
