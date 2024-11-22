9081b39637d084c443a4351933376d0204e5b549
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-22 17:00:05 -0300

    Implementando checkIn() em ParkingLotService

--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/services/ParkingLotService.java
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/services/ParkingLotService.java
@@ -1,9 +1,17 @@
 package com.pbcompass.park_api.services;
 
+import com.pbcompass.park_api.entities.Client;
+import com.pbcompass.park_api.entities.ClientParkingSpot;
 import com.pbcompass.park_api.entities.ParkingSpot;
+import com.pbcompass.park_api.utils.ParkingLotUtils;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static com.pbcompass.park_api.entities.ParkingSpot.StatusParkingSpot.OCCUPIED;
 
 @RequiredArgsConstructor
 @Service
@@ -17,4 +25,19 @@ public class ParkingLotService {
 
     @Autowired
     private final ParkingSpotService parkingSpotService;
+
+    @Transactional
+    public ClientParkingSpot checkIn(ClientParkingSpot clientParkingSpot) {
+        Client client = clientService.findByCpf(clientParkingSpot.getClient().getCpf());
+        clientParkingSpot.setClient(client);
+
+        ParkingSpot parkingSpot = parkingSpotService.findAvailableSpot();
+        parkingSpot.setStatus(OCCUPIED);
+        clientParkingSpot.setParkingSpot(parkingSpot);
+
+        clientParkingSpot.setEntryDate(LocalDateTime.now());
+        clientParkingSpot.setReceipt(ParkingLotUtils.generateReceipt());
+
+        return clientParkingSpotService.insert(clientParkingSpot);
+    }
 }
--- API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/services/ParkingSpotService.java
+++ API REST e Spring Boot Aprenda do Zero e na Prática/Semana 6/park-api/src/main/java/com/pbcompass/park_api/services/ParkingSpotService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import static com.pbcompass.park_api.entities.ParkingSpot.StatusParkingSpot.AVAILABLE;
+
 @Service
 @RequiredArgsConstructor
 public class ParkingSpotService {
@@ -32,4 +34,10 @@ public class ParkingSpotService {
         );
     }
 
+    @Transactional
+    public ParkingSpot findAvailableSpot() {
+        return parkingSpotRepository.findFirstByStatus(AVAILABLE).orElseThrow(
+                () -> new EntityNotFoundException("Available parking spot not found")
+        );
+    }
 }
