d35f535ff1f7c2dc3dfd2ed5885856e978de4c41
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-12-04 15:44:43 -0300

    Implementando repository, entity, service e controller de client

---
+++ Microservicos e Mensageria com Spring Cloud e Docker/Semana 9/msclients/src/main/java/com/pbcompass/msclients/services/ClientService.java
@@ -0,0 +1,29 @@
+package com.pbcompass.msclients.services;
+
+import com.pbcompass.msclients.domain.Client;
+import com.pbcompass.msclients.repositories.ClientRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClientService {
+
+    @Autowired
+    private final ClientRepository repository;
+
+    @Transactional
+    public Client save(Client client){
+        return repository.save(client);
+    }
+
+    public Optional<Client> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+
+}
