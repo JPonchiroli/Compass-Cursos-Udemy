23caa6adff62bbb43a331378e8c8886dd1330f29
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-12-05 17:07:23 -0300

    adicionando base mscards

---
+++ Microservicos e Mensageria com Spring Cloud e Docker/Semana 9/mscards/src/test/java/com/pbcompass/mscards/MscardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbcompass.mscards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
