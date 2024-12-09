dea5f6d1e2b3592c08ceaef6c697cd8255631ca2
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-12-09 15:37:17 -0300

    Implementando base projeot do 0 a AWS

---
+++ REST API's RESTFul do 0 à AWS c. Spring Boot 3 Java e Docker/Semana 10/startUp/src/main/java/com/pbcompass/startUp/StartUpApplication.java
@@ -0,0 +1,13 @@
+package com.pbcompass.startUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpApplication.class, args);
+	}
+
+}
---
+++ REST API's RESTFul do 0 à AWS c. Spring Boot 3 Java e Docker/Semana 10/startUp/src/test/java/com/pbcompass/startUp/StartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbcompass.startUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
