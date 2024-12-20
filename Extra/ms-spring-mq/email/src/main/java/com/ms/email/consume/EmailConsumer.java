814b4882c5d5c17ee08f05647c2ca062bab7e6ef
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-12-20 17:04:14 -0300

    feature: implementing the connection with rabbitmq

---
+++ Extra/ms-spring-mq/email/src/main/java/com/ms/email/consume/EmailConsumer.java
@@ -0,0 +1,14 @@
+package com.ms.email.consume;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listenEmail(@Payload String email) {
+        System.out.println(email);
+    }
+}
